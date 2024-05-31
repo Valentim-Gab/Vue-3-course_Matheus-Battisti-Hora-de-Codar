9f40f4ef4ed0ae08f9d15154f2d91bb72656f78c
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2024-05-31 16:23:36 -0300

    Curso de Vue 3: #18 - Emit

---
+++ 2-vue-project/src/components/BtnChangePicture.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+defineEmits(['handleChangePicture'])
+</script>
+
+<template>
+  <button @click="$emit('handleChangePicture')">Trocar imagem de perfil</button>
+</template>
+
+<style lang="scss"></style>
--- 2-vue-project/src/components/PictureComponent.vue
+++ 2-vue-project/src/components/PictureComponent.vue
@@ -1,15 +1,22 @@
 <script setup lang="ts">
-import ReusableComponent from './ReusableComponent.vue'
+import { ref } from 'vue'
+import BtnChangePicture from './BtnChangePicture.vue'
 
-const avatar = '/img/avatar.png'
+const avatar = ref('/img/avatar.png')
 const description = 'Kenny Steam'
+
+function handleChangeImage() {
+  avatar.value =
+    avatar.value == '/img/avatar.png'
+      ? (avatar.value = '/img/avatar2.png')
+      : (avatar.value = '/img/avatar.png')
+}
 </script>
 
 <template>
   <div>
     <img :src="avatar" :alt="description" width="100" />
-    <ReusableComponent />
-    <ReusableComponent />
+    <BtnChangePicture @handle-change-picture="handleChangeImage" />
   </div>
 </template>
 
