f8c7b9591acc01d078718dd893b04a40fcd9e7c3
Valentim-Gab <gabrielvalentimxavier@gmail.com>
2024-05-31 11:52:49 -0300

    Curso de Vue 3: #09 - Conhecendo as diretivas (v-if, v-show, v-for)

--- 2-vue-project/src/components/InfoPessoa.vue
+++ 2-vue-project/src/components/InfoPessoa.vue
@@ -1,14 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const isWorking = ref(true)
+const showEmail = ref(true)
+const email = ref('Valentim@email.vale')
+</script>
 
 <template>
   <div>
-    <p>Estou trabalhando no presencial.</p>
+    <p v-if="isWorking">Estou trabalhando no presencial.</p>
+    <p v-else>Estou em busca de novas oportunidades!</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>Vue</li>
       <li>TypeScript</li>
       <li>Delphi</li>
     </ul>
+    <p v-show="showEmail">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
